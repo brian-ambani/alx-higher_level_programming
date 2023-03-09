3b69fe224461264d7e327e4526355190761870b0
brian-ambani <brainambani1@gmail.com>
2023-03-09 00:56:37 -0800

    it doesnt work to checked later

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if "__name__" == "__main__":
+    import sys
+    add = 0
+    for value in sys.argv:
+        if value != sys.argv[0]:
+            add +=int(sys.argv[value])
+    print("{}".format(add))
