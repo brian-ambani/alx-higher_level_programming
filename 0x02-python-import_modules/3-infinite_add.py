21f4b5d1270a3759f1f57bf12b60b2c7e4118673
brian-ambani <brainambani1@gmail.com>
2023-03-09 01:17:32 -0800

    updated task-3 and it works

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-if "__name__" == "__main__":
+if __name__ == "__main__":
     import sys
     add = 0
     for value in sys.argv:
         if value != sys.argv[0]:
-            add +=int(sys.argv[value])
+            add +=int(value)
     print("{}".format(add))
