ff892db6d333105662f90be55ad3196e2f89ff52
brian-ambani <brainambani1@gmail.com>
2023-03-28 15:24:47 -0700

    updated task-2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,22 +1,10 @@
 #!/usr/bin/python3
-""" A module that defines a square
-"""
-
-
-class square:
+class Square:
     """ A class that defines a square by its size
     """
-
     def __init__(self, size=0):
-        """ A method to initialize the square
-        Args:
-            size(int): this is the size of the square
-
-        Raises:
-            TypeError: If the size is not an integer
-            ValueError: If the size is less than zero
+        """ Method to initialize the square object
         """
-
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
