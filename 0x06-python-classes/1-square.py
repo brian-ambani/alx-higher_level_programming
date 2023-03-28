125aafd3f67ecd97627a9af76639b28203a9853d
brian-ambani <brainambani1@gmail.com>
2023-03-28 14:15:01 -0700

    updated task-1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 class Square:
-    """Class Square that defines a square object
+    """Defines a square
     """
     def __init__(self, size):
         """Initialize method that stores the size of the square
