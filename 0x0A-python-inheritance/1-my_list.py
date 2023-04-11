9c0596e2ab4c3668e0d50aca60bbf3a6035473bb
brian-ambani <brainambani1@gmail.com>
2023-04-11 04:50:39 -0700

    incomplete task-1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+A module containing a class MyList that
+inherits from list
+"""
+
+class MyList(list):
+    """
+    MyList class that inherits 
+    from list
+
+    Args:
+        list:
+            parent class to MyList
+
+    """
+
+
+    def print_sorted(self):
+        """
+        Method responsible for printing
+        a sorted list
+        """
+
