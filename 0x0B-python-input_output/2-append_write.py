590eb242391b08e244972d1a28bd4f875325d0ba
brian-ambani <brainambani1@gmail.com>
2023-04-12 13:00:58 -0700

    updated task-2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,23 +3,14 @@
 """
 
 
-def read_lines(filename="", nb_lines=0):
-    """ Function that reads from a file and prints its number of lines
+def append_write(filename="", text=""):
+    """
+    A function that appends a string at the end
+    of a text file
     Args:
-        filename: filename
-        nb_lines: number of lines to print
-    Raises
-        Exception: when the file can be opened
+        filename: name of the file
+        text: text in the text file
     """
 
-    with open(filename, 'r', encoding="utf-8") as f:
-        if nb_lines <= 0:
-            read_data = f.read()
-            print(read_data, end='')
-        else:
-            n_lines = 0
-            for line in f:
-                print(line, end='')
-                n_lines += 1
-                if n_lines == nb_lines:
-                    break
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
