9426a6337ac9d5c8e27bc51c0ef4272ba2e6a549
brian-ambani <brainambani1@gmail.com>
2023-04-18 19:27:27 +0300

    updated base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -14,3 +14,12 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """
+        Method that initializes instances
+        """
+
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
