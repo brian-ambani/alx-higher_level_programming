9d4a2611bbf2e357bff457584e54425b432a8b8e
brian-ambani <brainambani1@gmail.com>
2023-04-18 15:07:59 +0300

    updated the base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module contains the base class
+for other classes
+"""
+
+
+class Base:
+    """
+    Represents the base class for
+    the other classes
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
