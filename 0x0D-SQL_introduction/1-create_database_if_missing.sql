21d25a16b74d29a696fe20b20e6e288a5ff24108
brian-ambani <brainambani1@gmail.com>
2023-05-18 11:13:46 -0700

    updated task-0

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,4 +1,4 @@
--- a script that creates creates the database hbtn_0c_0 in MySQL server
+-- Creates creates the database hbtn_0c_0 in MySQL server
 --- Does fail if the database exists
 
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
