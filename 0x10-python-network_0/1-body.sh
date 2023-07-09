f9fe9d298ee10e6ad203f56c0b68d97de9094ef8
brian-ambani <brainambani1@gmail.com>
2023-07-09 09:45:59 -0700

    updated task-1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# send a DELETE request to an URL with curl and display the body of the response
-curl -sX DELETE "$1"
+# Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
+curl -sL "$1"
